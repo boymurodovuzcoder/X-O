d61108e51c525c55ddae97349cddb1321b920618
boymurodovuzcoder <boymurodovuzcoder@gmail.com>
2021-02-10 07:03:31 +0500

    project is partly finished. Task is to indicate winner

--- xo/XO/src/pages/Index.vue
+++ xo/XO/src/pages/Index.vue
@@ -27,7 +27,8 @@ export default {
   data () {
     return {
       xIds: [],
-      yIds: [0, 1, 2, 3, 4, 5, 6, 7, 8]
+      oIds: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+      game: false
     }
   },
   methods: {
@@ -37,39 +38,70 @@ export default {
     },
     DrawX: function (event) {
       var id = event.currentTarget.id
-      document.getElementById(id).textContent = 'X'
-      this.xIds.push(id)
+      var grid = document.getElementById(id)
+      if (!this.game) {
+        if (!grid.textContent) {
+          grid.textContent = 'X'
+          this.xIds.push(Number(id))
+        }
+      }
     },
     DrawO: function () {
-      //  parsing ecach string number to number
-      var i
-      for (i in this.xIds) {
-        this.xIds[i] = Number(this.xIds[i])
-      }
-
-      console.log('x s: ', this.xIds)
-
-      // !intersection
-      var self = this
-      var extract = (num) => {
-        if (!self.xIds.includes(num)) return num
-      }
-      this.yIds = this.yIds.filter(extract)
-      // choosing random element from yIds
-      var restFunc = () => {
-        var randomElement = this.rand(this.yIds)
+      this.xIds.forEach((item) => {
+        if (this.oIds.includes(item)) {
+          var indexItem = this.oIds.indexOf(item)
+          if (indexItem !== -1) {
+            this.oIds.splice(indexItem, 1)
+          }
+        }
+      })
+      var randomElement = this.rand(this.oIds)
+      if (!this.game) {
         document.getElementById(randomElement.toString()).textContent = 'O'
-        console.log('rand:', randomElement)
-        this.yIds.pop(4)
-        console.log('y s: ', this.yIds)
-        console.log('')
       }
-      setTimeout(restFunc, 500)
+
+      var index = this.oIds.indexOf(randomElement)
+      if (index !== -1) {
+        this.oIds.splice(index, 1)
+      }
+      console.log(this.oIds)
+    },
+    checkState: function () {
+      var td = document.querySelectorAll('td')
+      const allEqual = arr => arr.every(v => v === arr[0])
+      var rows = [[td[0].textContent, td[1].textContent, td[2].textContent], [td[3].textContent, td[4].textContent, td[5].textContent], [td[6], td[7], td[8]]]
+      var columns = [[td[0].textContent, td[3].textContent, td[6].textContent], [td[1].textContent, td[4].textContent, td[7].textContent], [td[2].textContent, td[5].textContent, td[8].textContent]]
+      var diagonals = [[td[0].textContent, td[4].textContent, td[8].textContent], [td[2].textContent, td[4].textContent, td[6].textContent]]
+
+      var i
+      for (i in rows) {
+        if ((allEqual(rows[i])) && (rows[i][0] !== '')) {
+          console.log('game ended')
+          this.game = true
+          //  decoration
+        }
+      }
+      for (i in columns) {
+        if ((allEqual(columns[i])) && (columns[i][0] !== '')) {
+          console.log('game ended')
+          this.game = true
+        }
+      }
+      for (i in diagonals) {
+        if ((allEqual(diagonals[i])) && (diagonals[i][0] !== '')) {
+          console.log('game ended')
+          this.game = true
+        }
+      }
     }
   },
   watch: {
-    xIds: function (val) {
-      this.DrawO()
+    xIds: function (newVal, oldVal) {
+      setTimeout(this.DrawO, 1000)
+      this.checkState()
+    },
+    oIds: function (newVal, oldVal) {
+      this.checkState()
     }
   }
 }
